aa7ae8df068d3e456a6dffcd555928644917eb8c
NguyenNgocThach <thachnguyenngoc2504@gmail.com>
2024-03-12 15:41:17 +0700

    Update web.php

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -19,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [userController::class, 'index']);
+Route::get('/about', [userController::class, 'about']);
+Route::get('/contract', [userController::class, 'contract']);
+Route::get('/hosting', [userController::class, 'hosting']);
+Route::get('/domain', [userController::class, 'domain']);
+Route::get('/login', [userController::class, 'login']);
+
+
+
