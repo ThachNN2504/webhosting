42a86f1117b07348e43f050a6acf7974306ea832
NguyenNgocThach <thachnguyenngoc2504@gmail.com>
2024-03-12 15:29:34 +0700

    Update userController.php

--- Web/app/Http/Controllers/userController.php
+++ Web/app/Http/Controllers/userController.php
@@ -9,4 +9,20 @@ class userController extends Controller
     function index(){
         return view('index');
     }
+    function about(){
+        return view('about');
+    }
+    function contract(){
+        return view('contract');
+    }
+    function hosting(){
+        return view('hosting');
+    }
+
+    function domain(){
+        return view('domain');
+    }
+    function login(){
+        return view('login');
+    }
 }
